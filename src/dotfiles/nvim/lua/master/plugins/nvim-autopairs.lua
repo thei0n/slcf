d7a154dde4213ff7bd000f4b10101b02c6e718a9
theion <pheion1@proton.me>
2024-02-11 19:35:06 +0400

    nvim: autocomment on conf filetype is ignored

--- src/dotfiles/nvim/lua/master/plugins/nvim-autopairs.lua
+++ src/dotfiles/nvim/lua/master/plugins/nvim-autopairs.lua
@@ -1,5 +1,5 @@
 require('nvim-autopairs').setup({
-	 disable_filetype = { "TelescopePrompt", "spectre_panel", "neo-tree-popup"},
+	 disable_filetype = { "TelescopePrompt", "spectre_panel", "neo-tree-popup", "conf"},
 	 disable_in_macro = true,
 	 disable_in_visualblock = false,
 	 disable_in_replace_mode = true,
