bcb356967a69809e2e0b320ae3c68bd8a347a494
theion <farajli@hotmail.com>
2024-02-26 16:53:51 +0400

    upgrade to /dotfiles/nvim

--- src/dotfiles/nvim/lua/master/vim_settings.lua
+++ src/dotfiles/nvim/lua/master/vim_settings.lua
@@ -1,28 +1,29 @@
 -- [[Vim settings]]
 
-vim.cmd("set clipboard+=unnamedplus") --Use System Clipboard
+vim.cmd("set clipboard+=unnamedplus") -- Use System Clipboard
 vim.opt.fillchars = {eob = " "}
 
 
 -- [[ Changes to make vim look simpler ]]
-vim.cmd([[ set shortmess+=I ]]) --Remove Intro text when entering vim
-vim.cmd("set noshowmode") 
+vim.cmd([[ set shortmess+=I ]]) -- Remove Intro text when entering vim
+vim.cmd("set noshowmode")
 -- vim.cmd("set noshowcmd")
 vim.cmd("set noruler")
 vim.cmd("set laststatus=-")
 vim.cmd("set statusline=0")
 -- []
 
-vim.cmd([[set number]]) 
+vim.cmd([[set number]])
 
-vim.cmd([[set scrolloff=5]]) --Smooth scroll
+vim.cmd([[set scrolloff=5]]) -- Smooth scroll
 
 vim.o.background = "dark" -- or "light" for light mode
 
-vim.cmd([[set formatoptions-=cro]]) --deactivating autocomment 
+vim.cmd([[set formatoptions-=cro]]) -- Deactivating autocomment
 
 vim.cmd([[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]])
 
+vim.cmd([[ autocmd BufwritePost * %s/\s\+$//e ]]) -- Remove useless spaces when saving
 -- [[Mappings]]
 
 -- Move cursor in Insert mode
